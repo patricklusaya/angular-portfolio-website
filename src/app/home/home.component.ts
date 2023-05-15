d02e9714a3706507756664c28f307c35295549fb
Patrick Lusaya <lusayapatrick@gmail.com>
2023-05-15 11:42:58 +0300

    profile updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  list = ['Flutter & Angular for frontend','java Spring boot for backend']
-  tech = ['Angular','Flutter','Spring boot','Python','SQL',]
-  tools=['Figma', 'Wordpress', 'SPSS','Ms Excel', 'KOBO Toolbox']
+  list = ['Flutter & Angular for frontend','java Spring boot & Laravel for backend']
+  tech = ['Angular','Flutter','Spring boot','PHP','SQL',]
+  tools=['Figma', 'Wordpress',  'Vs Code', 'CapCut']
   constructor() { }
 
   ngOnInit(): void {
